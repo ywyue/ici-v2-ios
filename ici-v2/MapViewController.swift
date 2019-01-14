13070eef20771631989f228315e4b429334045e2
ywyue <865051699@qq.com>
2019-01-14 22:29:28 +0800

    second commit

--- ici-v2/MapViewController.swift
+++ ici-v2/MapViewController.swift
@@ -10,16 +10,68 @@ import UIKit
 import MapKit
 
 class MapViewController: UIViewController , MAMapViewDelegate{
-
+    
+    class MAPoiAnnotation: MAPointAnnotation{ //poi图标类（为了和自己的位置图标区分）
+    }
+    var topicPopupView: UIView! //热门话题弹窗视图
+    var topicImageView: UIImageView! //热门话题图标
+    var topicLabel: UILabel! //热门话题label
+    var watchImageView: UIImageView! //围观图标
+    var watchLabel: UILabel! //围观label
+    var topicContentLabel: UILabel! //热门话题内容label
+    
+    var contentPopupView: UIView! //热门内容弹窗视图
     override func viewDidLoad() {
         super.viewDidLoad()
+        /* 热门话题弹窗视图 */
+        topicPopupView = UIView(frame:CGRect(x:self.view.frame.width/2-335.px(),y:self.view.frame.height-338.px(),width:670.px(),height:150.px()))
+        topicPopupView.backgroundColor = UIColor(patternImage: UIImage(named: "topicPopup")!)
+        
+        /* 热门话题图标 */
+        topicImageView = UIImageView(image: UIImage(named: "bulb"))
+        topicImageView.frame = CGRect(x:33.px(),y:20.px(),width:96.px(),height:96.px())
+        topicPopupView.addSubview(topicImageView)
+        
+        /* 热门话题label */
+        topicLabel = UILabel()
+        topicLabel.frame = CGRect(x:162.px(),y:35.px(),width:150.px(),height:30.px())
+        topicLabel.textAlignment = .left
+        topicLabel.font = UIFont.boldSystemFont(ofSize:30.px())
+        topicLabel.text = "热门话题"
+        topicLabel.textColor = UIColor.black
+        topicPopupView.addSubview(topicLabel)
+        
+        /* 热门话题内容label */
+        topicContentLabel = UILabel()
+        topicContentLabel.frame = CGRect(x:162.px(),y:79.px(),width:300.px(),height:25.px())
+        topicContentLabel.textAlignment = .left
+        topicContentLabel.font = UIFont.boldSystemFont(ofSize:25.px())
+        topicContentLabel.text = "这里会举行金秋艺术节"
+        topicContentLabel.textColor = UIColor.gray
+        topicPopupView.addSubview(topicContentLabel)
+        
+        /* 围观图标 */
+        watchImageView = UIImageView(image: UIImage(named: "watchBt"))
+        watchImageView.frame = CGRect(x:490.px(),y:39.px(),width:130.px(),height:60.px())
+        
+        /* 围观label */
+        watchLabel = UILabel()
+        watchLabel.frame = CGRect(x:watchImageView.frame.width/2-32.5.px(),y:watchImageView.frame.height/2-15.px(),width:65.px(),height:30.px())
+        watchLabel.textAlignment = .center
+        watchLabel.font = UIFont.boldSystemFont(ofSize:30.px())
+        watchLabel.text = "围观"
+        watchLabel.textColor = UIColor.black
+        watchImageView.addSubview(watchLabel)
+        topicPopupView.addSubview(watchImageView)
+        
+        
         
 //        self.title = "地图"
 //        self.tabBarItem.image = UIImage(named: "MapIcon")?.withRenderingMode(.alwaysOriginal)
         
         let mapView = MAMapView(frame: self.view.bounds)
         var path = Bundle.main.bundlePath
-        let pointAnnotation = MAPointAnnotation()
+        let pointAnnotation = MAPoiAnnotation()
         path.append("/style.data")
         let jsonData = NSData.init(contentsOfFile: path)
         mapView.setCustomMapStyleWithWebData(jsonData as Data!)
@@ -50,6 +102,7 @@ class MapViewController: UIViewController , MAMapViewDelegate{
         mapView.addAnnotation(pointAnnotation)
         
         self.view.addSubview(mapView)
+        self.view.addSubview(topicPopupView)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -58,7 +111,7 @@ class MapViewController: UIViewController , MAMapViewDelegate{
 extension MapViewController {
     func mapView(_ mapView: MAMapView!, viewFor annotation: MAAnnotation!) -> MAAnnotationView! {
         
-        if annotation.isKind(of: MAPointAnnotation.self) {
+        if (annotation is MAPoiAnnotation) {  //判断是否为poi点
             let pointReuseIndetifier = "item"
             var annotationView: MAAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: pointReuseIndetifier)
             
@@ -76,6 +129,5 @@ extension MapViewController {
         return nil
     }
 }
-
 
 
